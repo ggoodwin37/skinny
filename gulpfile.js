bad54e3e0b07820d1b3fa95947747d3684b8f0e6
ggoodwin37 <ggoodwin37@gmail.com>
2016-04-05 15:26:43 -0700

    React works.

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,47 @@
 'use strict';
 
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var dest = require('gulp-dest');
-var browserify = require('browserify');
-var reactify = require('reactify');
-var watchify = require('watchify');
-var source = require('vinyl-source-stream');
-var less = require('gulp-less');
-
 // based on http://christianalfoni.github.io/javascript/2014/08/15/react-js-workflow.html
 
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var source = require('vinyl-source-stream');
+var dest = require('gulp-dest');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var less = require('gulp-less');
+
+var outPath = './dist';
+
 gulp.task('browserify', function() {
-    var bundler = browserify({
-        entries: ['./client/app.jsx'],
-        transform: [reactify],  // or babelify?
-        debug: true,
-        cache: {},
-        packageCache: {},
-        fullPaths: true
-    });
-    function doBundle(watcher) {
-        // could do uglifying after source pipe
-        console.log('Updating js bundle...');
-        watcher
+    function doBundle() {
+        console.log('Bundling js...');
+        browserify('./client/app.jsx')
+            .transform(babelify, {presets: ['es2015', 'react']})
             .bundle()
-            .pipe(source('app.jsx'))
+            .pipe(source('app.js'))
             .pipe(dest({ext: 'js'}))
-            .pipe(gulp.dest('./dist'));
-        return watcher;
+            .pipe(gulp.dest(outPath));
     }
-    var watcher = watchify(bundler)
-        .on('update', function() {
-            return doBundle(watcher);
-        });
-    return doBundle(watcher);
+
+    var jsFilePath = 'client/*.js*';
+    gulp.watch(jsFilePath, doBundle);
+    doBundle();
 });
 
 gulp.task('less', function() {
     var lessFilePath = 'less/**/*.less';
-    var lessOutPath = './dist';
     function doLess() {
-        console.log('Transforming less source...');
+        console.log('Transforming less...');
         return gulp.src(lessFilePath)
             .pipe(less({
                 // paths: [path.join(__dirname, 'less', 'includes')]
             }))
             .pipe(concat('app.css'))
-            .pipe(gulp.dest(lessOutPath));
+            .pipe(gulp.dest(outPath));
     }
-    gulp.watch(lessFilePath, function() {
-        return doLess();
-    });
-    return doLess();
+
+    gulp.watch(lessFilePath, doLess);
+    doLess();
 });
 
 gulp.task('default', ['browserify', 'less']);
