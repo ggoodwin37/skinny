ecf7a28fbbe528e9c9648d488bfcdb5186ed8721
ggoodwin37 <ggoodwin37@gmail.com>
2016-04-05 13:23:48 -0700

    Tabs -> spaces.

--- gulpfile.js
+++ gulpfile.js
@@ -8,38 +8,38 @@ var less = require('gulp-less');
 // see http://christianalfoni.github.io/javascript/2014/08/15/react-js-workflow.html
 
 gulp.task('browserify', function() {
-	var bundler = browserify({
-		entries: ['./client/app.js'],
-		transform: [reactify],  // or babelify?
-		debug: true,
-		cache: {},
-		packageCache: {},
-		fullPaths: true
-	});
-	var watcher = watchify(bundler);
-	return watcher
-		.on('update', function() {
-			var updateStart = Date.now();
-			console.log('Updating!');
-			watcher.bundle()
-			    .pipe(source('main.js'))
-				// This is where you add uglifying etc.
-			    .pipe(gulp.dest('./build/'));
-			console.log('Updated!', (Date.now() - updateStart) + 'ms');
-		})
-		.bundle()
-		.pipe(source('main.js'))
-		.pipe(gulp.dest('./dist'));
+    var bundler = browserify({
+        entries: ['./client/app.js'],
+        transform: [reactify],  // or babelify?
+        debug: true,
+        cache: {},
+        packageCache: {},
+        fullPaths: true
+    });
+    var watcher = watchify(bundler);
+    return watcher
+        .on('update', function() {
+            var updateStart = Date.now();
+            console.log('Updating!');
+            watcher.bundle()
+                .pipe(source('main.js'))
+                // This is where you add uglifying etc.
+                .pipe(gulp.dest('./build/'));
+            console.log('Updated!', (Date.now() - updateStart) + 'ms');
+        })
+        .bundle()
+        .pipe(source('main.js'))
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('less', function() {
-	gulp.watch('./less/**/*.less', function() {
-		return gulp.src('./less/**/*.less')
-			.pipe(less({
-				// paths: [path.join(__dirname, 'less', 'includes')]
-			}))
-			.pipe(gulp.dest('./dist/css'))
-	});
+    gulp.watch('./less/**/*.less', function() {
+        return gulp.src('./less/**/*.less')
+            .pipe(less({
+                // paths: [path.join(__dirname, 'less', 'includes')]
+            }))
+            .pipe(gulp.dest('./dist/css'))
+    });
 });
 
 gulp.task('default', ['browserify', 'less']);
--- server-instance.js
+++ server-instance.js
@@ -4,37 +4,37 @@ var inspect = require('eyes').inspector({maxLength: null});
 
 function startServerInstance(done) {
 
-	var app = {
-		config: config,
-		inspect: inspect
-	};
+    var app = {
+        config: config,
+        inspect: inspect
+    };
 
-	var server = new hapi.Server();
-	server.connection({
-		host: config.serverHost,
-		port: config.serverPort
-	});
+    var server = new hapi.Server();
+    server.connection({
+        host: config.serverHost,
+        port: config.serverPort
+    });
 
-	server.route({
-		method: 'GET',
-		path: '/',
-		handler: function (request, reply) {
-			reply('<a href="/s/test">test</a>');
-		}
-	});
-	server.route({
-		method: 'GET',
-		path: '/s/{name*}',
-		handler: function (request, reply) {
-			// TODO: best way to return an html page with links here?
-			reply('<h1>Hi ' + encodeURIComponent(request.params.name) + ' :D</h1>');
-		}
-	});
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+            reply('<a href="/s/test">test</a>');
+        }
+    });
+    server.route({
+        method: 'GET',
+        path: '/s/{name*}',
+        handler: function (request, reply) {
+            // TODO: best way to return an html page with links here?
+            reply('<h1>Hi ' + encodeURIComponent(request.params.name) + ' :D</h1>');
+        }
+    });
 
-	server.start(function () {
-		console.log('app is running at', server.info.uri);
-	});
-	return server;
+    server.start(function () {
+        console.log('app is running at', server.info.uri);
+    });
+    return server;
 }
 
 module.exports = startServerInstance;
