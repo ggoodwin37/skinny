c71ee2385e802597baa7c732591f7a82b0ef4408
ggoodwin37 <ggoodwin37@gmail.com>
2016-04-05 13:30:46 -0700

    Refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var watchify = require('watchify');
-
 var less = require('gulp-less');
-//var path = require('path');
 
 // see http://christianalfoni.github.io/javascript/2014/08/15/react-js-workflow.html
 
@@ -16,20 +14,22 @@ gulp.task('browserify', function() {
         packageCache: {},
         fullPaths: true
     });
-    var watcher = watchify(bundler);
-    return watcher
+    function doBundle(watcher) {
+        // could do uglifying after source pipe
+        var updateStart = Date.now();
+        console.log('Updating bundle...');
+        watcher
+            .bundle()
+            .pipe(source('app.js'))
+            .pipe(gulp.dest('./dist'));
+        console.log('Done updatin\' bundle, took ' + (Date.now() - updateStart) + 'ms');
+        return watcher;
+    }
+    var watcher = watchify(bundler)
         .on('update', function() {
-            var updateStart = Date.now();
-            console.log('Updating!');
-            watcher.bundle()
-                .pipe(source('main.js'))
-                // This is where you add uglifying etc.
-                .pipe(gulp.dest('./build/'));
-            console.log('Updated!', (Date.now() - updateStart) + 'ms');
-        })
-        .bundle()
-        .pipe(source('main.js'))
-        .pipe(gulp.dest('./dist'));
+            return doBundle(watcher);
+        });
+    return doBundle(watcher);
 });
 
 gulp.task('less', function() {
