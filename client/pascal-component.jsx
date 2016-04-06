9af8d3275b2cc009ecdf772f3464551c4e612e8f
ggoodwin37 <ggoodwin37@gmail.com>
2016-04-06 16:39:20 -0700

    Styling.

--- client/pascal-component.jsx
+++ client/pascal-component.jsx
@@ -1,9 +1,15 @@
 var React = require('react');
+var classNames = require('classnames');
 
 var testComponent = React.createClass({
     render: function() {
+        var title = 'This is the Pascal\'s triangle component.';
+        var descr = 'Show Pascal\'s triangle, using flexbox, and highlight odd numbers to look for patterns.';
         return (
-            <div><h1>This is the pascal triangle component</h1><span>TODO.</span></div>
+                <div className={classNames('s-component', 'pascal-component')}>
+                    <h1 className="title">{title}</h1>
+                    <div className="description">{descr}</div>
+                </div>
         );
     }
 });
