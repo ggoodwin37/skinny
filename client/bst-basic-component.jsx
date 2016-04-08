a81bf03508a8858f3bc34b94f6e5ee94bcbec738
ggoodwin37 <ggoodwin37@gmail.com>
2016-04-07 17:42:53 -0700

    Hooking up bst, worked first try :D

--- client/bst-basic-component.jsx
+++ client/bst-basic-component.jsx
@@ -4,14 +4,32 @@ var classNames = require('classnames');
 var LogOutput = require('./log-output-component.jsx');
 var randomString = require('./random-string');
 
+var binSortTree = require('./bin-tree').binSortTree;
+
 var BstBasicComponent = React.createClass({
+    testBst: function() {
+        const numEntries = 17;
+        var msgs = [], inserted = [], i, str;
+        var tree = new binSortTree();
+        for (i = 0; i < numEntries; ++i) {
+            str = randomString();
+            tree.insert(str);
+            inserted.push(str);
+        }
+        msgs.push('Inserted ' + numEntries + ' strings into bst: ' + inserted.join(','));
+
+        var traversed = [];
+        tree.inOrder((val) => {
+            traversed.push(val);
+        });
+        msgs.push('In-order traversal of bst is: ' + traversed.join(','));
+        msgs.push('Length of in-order traversal is: ' + traversed.length);
+        return msgs;
+    },
     render: function() {
         var title = 'Basic binary-sort-tree shit.';
         var descr = 'BST insert and in-order traversal.';
-
-        // two ways to use logger: either run stuff first and accumlate output logs in this init array,
-        // or wait until after render and log stuff dynamically to this.logger (set via ref hook).
-        var initialLogMessages = []; // could just run all bst code first and accumulate logs, then output.
+        var initialLogMessages = this.testBst();
         return (
                 <div className={classNames('s-component', 'bst-component')}>
                     <h1 className="title">{title}</h1>
