d57793aaf15714508d6bcce4b9ec167a95f70bc3
ggoodwin37 <ggoodwin37@gmail.com>
2016-04-08 13:42:32 -0700

    Highlight word, word terminator, and style tweaks.

--- client/trie-view-component.jsx
+++ client/trie-view-component.jsx
@@ -3,16 +3,28 @@ var classNames = require('classnames');
 
 var TrieNodeView = React.createClass({
     render: function() {
-        // TODO: support highlighting of a current word.
         var childKeys = Object.keys(this.props.node.children).sort();
         var childEls = childKeys.map(function(childKey) {
             var childNode = this.props.node.children[childKey];
+            var highlight = null;
+            if (this.props.highlight) {
+                console.log('node view highlight exists', this.props.highlight);
+                if (childKey === this.props.highlight.str[this.props.highlight.offs]) {
+                    highlight = {
+                        str: this.props.highlight.str,
+                        offs: this.props.highlight.offs + 1
+                    };
+                }
+            }
+            var childClassNames = classNames('trie-view-node-group',
+                                             {highlight: !!highlight,
+                                              term: childNode.isTerminator});
             return (
-                    <li className={classNames('trie-view-node-group')} key={childNode.id}>
+                    <li className={childClassNames} key={childNode.id}>
                         <div className={classNames('trie-view-node-label')}>
                             {childKey.toUpperCase()}
                         </div>
-                        <TrieNodeView node={childNode} />
+                        <TrieNodeView node={childNode} highlight={highlight}/>
                     </li>
             );
         }.bind(this));
@@ -27,8 +39,15 @@ var TrieNodeView = React.createClass({
 
 var TrieView = React.createClass({
     render: function() {
+        var highlight = null;
+        if (this.props.highlightWord) {
+            highlight = {
+                str: this.props.highlightWord,
+                offs: 0
+            }
+        }
         return (
-                <TrieNodeView node={this.props.trie.root} />
+                <TrieNodeView node={this.props.trie.root} highlight={highlight} />
         );
     }
 });
--- client/trie1-component.jsx
+++ client/trie1-component.jsx
@@ -20,6 +20,16 @@ var words = [
     'damaged',
     'critter',
     'cathedral',
+    'burrito',
+    'burr',
+    'fish',
+    'fix',
+    'fin',
+    'final',
+    'alter',
+    'alien',
+    'extreme',
+    'extraordinary',
     'a',
     'I',
     'zilch'
@@ -28,20 +38,21 @@ var words = [
 var Trie1Component = React.createClass({
     testTrie: function() {
         var testTrie = new trieDataStruct();
-        // for now, just insert a fixed list of words. Later we'll make this interactive.
+        // for now, just insert a fixed list of words. Later we could make this interactive.
         testTrie.insert(words);
         return testTrie;
     },
     render: function() {
+        var highlightWord = 'extreme';
         var testTrie = this.testTrie();
         var title = 'Trie.';
-        var descr = 'Prefix tree.';
+        var descr = 'Prefix tree. Red boxes are word terminations. Highlighting the word "' + highlightWord + '".';
         var msgs = [testTrie.toString()];
         return (
                 <div className={classNames('s-component', 'trie1-component')}>
                     <h1 className="title">{title}</h1>
                     <div className="description">{descr}</div>
-                    <TrieView trie={testTrie} />
+                    <TrieView trie={testTrie} highlightWord={highlightWord} />
                     <LogOutput initialMessages={msgs} />
                 <BackLinkComponent />
                 </div>
