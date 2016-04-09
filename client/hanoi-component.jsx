f8c8579b3019bafea60dafef0ca471c54e0a71b0
ggoodwin37 <ggoodwin37@gmail.com>
2016-04-09 13:20:08 -0700

    HanoiView

--- client/hanoi-component.jsx
+++ client/hanoi-component.jsx
@@ -4,13 +4,37 @@ var BackLinkComponent = require('./back-link-component.jsx');
 
 var hanoiDataStruct = require('./hanoi-data-struct');
 
+var HanoiStackView = React.createClass({
+    render: function() {
+        const stack = this.props.stack;
+        var pieceNodes = stack.map(function(piece) {
+            var classes = classNames('hanoi-piece', 'hanoi-piece-' + piece.val);
+            return (
+                    <span className={classes} key={piece.id}>{piece.val}</span>
+            );
+        });
+        return (
+                <div className={classNames('hanoi-stack')}>
+                    {pieceNodes}
+                </div>
+        );
+    }
+});
+
 var HanoiView = React.createClass({
     render: function() {
+        const hanoi = this.props.hanoi;
+        var stackNodes = hanoi.stacks.map(function(stack, i) {
+            var key = 'hanoi-stack-' + i;
+            return (
+                    <HanoiStackView stack={stack} key={key} />
+            );
+        });
         return (
-                <h1>TODO: HanoiView</h1>
+                <div className={classNames('hanoi-view')}>
+                    {stackNodes}
+                </div>
         );
-    },
-    componentDidMount: function() {
     }
 });
 
@@ -18,11 +42,14 @@ var HanoiComponent = React.createClass({
     render: function() {
         var title = 'Tower of Hanoi.';
         var descr = 'CS101 style.';
+        if (!this.hanoi) {
+            this.hanoi = new hanoiDataStruct();
+        }
         return (
                 <div className={classNames('s-component', 'hanoi-component')}>
                     <h1 className="title">{title}</h1>
                     <div className="description">{descr}</div>
-                    <HanoiView />
+                    <HanoiView hanoi={this.hanoi} />
                     <BackLinkComponent />
                 </div>
         );
--- client/hanoi-data-struct.js
+++ client/hanoi-data-struct.js
@@ -1,4 +1,14 @@
+var uuid = require('node-uuid');
+
+function hanoiPiece(val) {
+    this.val = val;
+    this.id = uuid.v1();
+}
 function hanoiDataStruct() {
+    this.stacks = [ [], [], [] ];
+    for (var i = 7; i >= 1; i--) {
+        this.stacks[0].push(new hanoiPiece(i));
+    }
 }
 
 module.exports = hanoiDataStruct;
