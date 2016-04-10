389074c2864bf3b1f1ee6549808c29c570ba3e71
ggoodwin37 <ggoodwin37@gmail.com>
2016-04-10 11:45:41 -0700

    Heap impl wip

--- client/heap-data-struct.js
+++ client/heap-data-struct.js
@@ -1,4 +1,25 @@
-function heapDataStruct() {
+// max heap
+function heapDataStruct(initialData) {
+    this.data = [];
+    for (var i = 0; i < initialData.length; ++i) {
+        this.insert(initialData[i]);
+    }
+}
+heapDataStruct.prototype.insert = function(val) {
+    this.data.push(initialData[i]);
+    this.shiftUp(this.data.length - 1);
+}
+heapDataStruct.prototype.shiftUp = function(index) {
+    if (index === 0) {
+        return;
+    }
+    var parentIndex = (index % 2) ? ((index - 1) / 2) : ((index - 2) / 2);
+    if (this.data[parentIndex] < this.data[index]) {
+        var temp = this.data[parentIndex];
+        this.data[parentIndex] = this.data[index];
+        this.data[index] = temp;
+        this.shiftUp(parentIndex);
+    }
 }
 
 module.exports = heapDataStruct;
