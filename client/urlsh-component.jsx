ec27f6452a37c8efd260aeeb1e739eb18f17fd09
ggoodwin37 <ggoodwin37@gmail.com>
2016-06-16 14:51:32 -0700

    Talk to API

--- client/urlsh-component.jsx
+++ client/urlsh-component.jsx
@@ -1,5 +1,7 @@
 var React = require('react');
 var classNames = require('classnames');
+var SimpleAjax = require('simple-ajax');
+
 var BackLinkComponent = require('./back-link-component.jsx');
 
 const componentTitle = 'URL shortener frontend.';
@@ -19,7 +21,7 @@ var UrlshComponent = React.createClass({
                     <h1 className="title">{componentTitle}</h1>
                     <div className="description">{componentDescription}</div>
                     <div className="urlsh-form">
-                        <form onSubmit={onSubmit} >
+                        <form onSubmit={onSubmit} autoComplete="off">
                             <label>URL to shorten:</label>
                             <input type="text" id="url-input" ref={(urlInputRef) => { this.urlInputRef = urlInputRef }} />
                             <input type="submit" value="Shorten" />
@@ -31,11 +33,31 @@ var UrlshComponent = React.createClass({
         );
     },
     postUrl: function(url) {
+        const apiKey = 'AIzaSyDEIbvw69tXpsOlhyOVTuILSjltioA9yVA';  // from dev console
+        const shortenerUrl = 'https://www.googleapis.com/urlshortener/v1/url?key=' + apiKey;
+        const postData = {
+            longUrl: url
+        };
+        const ajax = new SimpleAjax({
+            url: shortenerUrl,
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(postData)
+        });
+        ajax.on('success', (event) => {
+            const response = JSON.parse(event.target.response);
+            const result = response.id;
+            this.statusRef.innerText = "Result: " + result;
+        });
+        ajax.on('error', (event) => {
+            console.log('error', event);
+        });
+
         // TODO: make this slicker, probably have a new component render when we get the result, etc.
         this.statusRef.innerText = "Waiting for API";
-        window.setTimeout(() => {
-            this.statusRef.innerText = "Result: " + url;  // TODO
-        }, 5000);
+
+        // fire
+        ajax.send();
     },
 });
 
