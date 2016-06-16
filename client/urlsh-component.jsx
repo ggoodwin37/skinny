70bee259e605a975a6275cba84dda94201202911
ggoodwin37 <ggoodwin37@gmail.com>
2016-06-16 13:43:07 -0700

    URL input.

--- client/urlsh-component.jsx
+++ client/urlsh-component.jsx
@@ -6,15 +6,32 @@ const componentTitle = 'URL shortener frontend.';
 const componentDescription = 'Going through API consumer flow for urlshortener api.';
 var UrlshComponent = React.createClass({
     render: function() {
+        const self = this;
+        function onSubmit(e) {
+            e.preventDefault();
+            if (self.urlInputRef) {
+                const urlInput = self.urlInputRef.value;
+                self.postUrl(urlInput);
+            }
+        }
         return (
                 <div className={classNames('s-component', 'urlsh-component')}>
                     <h1 className="title">{componentTitle}</h1>
                     <div className="description">{componentDescription}</div>
-                    <div className="urlsh-form">Coming soon.</div>
+                    <div className="urlsh-form">
+                        <form onSubmit={onSubmit} >
+                            <label>URL to shorten:</label>
+                            <input type="text" id="url-input" ref={(urlInputRef) => { this.urlInputRef = urlInputRef }} />
+                            <input type="submit" text="Shorten" />
+                        </form>
+                    </div>
                     <BackLinkComponent />
                 </div>
         );
-    }
+    },
+    postUrl: function(url) {
+        console.log('posting url: ' + url);
+    },
 });
 
 module.exports = {
